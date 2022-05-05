85c095f0968bc93e988799bb7c2d2792abe6ad3c
Nataly Kurylov <natalyk@live.com>
2022-05-05 14:44:05 -0700

    messing around w/ navbar, will continue fixing

--- stingersign/src/components/ManageDocumentPage.js
+++ stingersign/src/components/ManageDocumentPage.js
@@ -4,7 +4,9 @@ import { Link } from "react-router-dom";
 
 export default function ManageDocumentPage() {
     return (
-      <main style={{ padding: "1rem 0" }}>
+      <div>
+      <div id="box3">
+      <main style={{ padding: "0.5rem 0" }}>
 
         <h2>Manage Documents</h2>
         <Link to="/homepage"style={{textDecoration:"none"}}>Home</Link> |{" "}
@@ -12,8 +14,9 @@ export default function ManageDocumentPage() {
         <Link to="/profile"style={{textDecoration:"none"}}>Profile</Link> |{" "}
         <Link to="/upload"style={{textDecoration:"none"}}>Upload</Link> |{" "}
         <Link to="/signature"style={{textDecoration:"none"}}>Signature Required</Link> |{" "}
-        <Link to="/ManageDocument"style={{textDecoration:"none"}}>Manage Documents</Link> |
-
+        <Link to="/ManageDocument"style={{textDecoration:"none"}}>Manage Documents</Link> 
       </main>
+      </div>
+      </div>
     );
   }
